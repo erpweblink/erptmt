1800cdf18c30d1c5061296b6e0aca5749b938a97
erpweblink <erpweblink@gmail.com>
2024-06-27 15:36:05 +0530

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,59 @@
+﻿<%@ Application Language="C#" %>
+<%@ Import Namespace="System.Timers" %>
+
+<script RunAt="server">
+
+    void Application_Start(object sender, EventArgs e)
+    {
+        //Class1 obj=new Class1();
+
+        //System.Timers.Timer myTimer = new System.Timers.Timer();
+        //// Set the Interval to 5 seconds (5000 milliseconds).
+        //myTimer.Interval = 300000; // 5mins
+        //myTimer.AutoReset = true;
+        //myTimer.Elapsed += new ElapsedEventHandler(myTimer_Elapsed);
+        //myTimer.Enabled = true; 
+    }
+
+    public void myTimer_Elapsed(object source, System.Timers.ElapsedEventArgs e)
+    {
+        //clsScheduleMail objScheduleMail = new clsScheduleMail();
+        //objScheduleMail.SendScheduleMail();
+    }
+
+    void Application_End(object sender, EventArgs e)
+    {
+        //  Code that runs on application shutdown
+
+    }
+
+    void Application_Error(object sender, EventArgs e)
+    {
+        // Code that runs when an unhandled error occurs
+
+    }
+
+    void Session_Start(object sender, EventArgs e)
+    {
+        // Code that runs when a new session is started
+        //if (Session["ProductionName"] == null)
+        //{
+        //    //Redirect to Welcome Page if Session is not null    
+        //  Response.Redirect("Login.aspx");
+        //}
+    }
+
+    void Session_End(object sender, EventArgs e)
+    {
+
+        // Code that runs when a session ends. 
+        // Note: The Session_End event is raised only when the sessionstate mode
+        // is set to InProc in the Web.config file. If session mode is set to StateServer 
+        // or SQLServer, the event is not raised.
+    }
+
+
+
+
+
+</script>
---
+++ LoginPage.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class LoginPage : System.Web.UI.Page
+{
+    SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["constr"].ConnectionString);
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            Session.Abandon();
+            FormsAuthentication.SignOut();
+            Session["Username"] = null;
+        }
+    }
+
+    protected void btnlogin_Click(object sender, EventArgs e)
+    {
+        //Cls_Main.Conn_Open();
+        SqlCommand cmd = new SqlCommand("SELECT * FROM TblUserMaster WHERE Username='" + txtusername.Text + "' AND Password='" + txtpassword.Text + "'", con);
+        cmd.CommandType = CommandType.Text;
+        cmd.Parameters.AddWithValue("@Username", txtusername.Text.Trim());
+        cmd.Parameters.AddWithValue("@Password", txtpassword.Text.Trim());
+        cmd.Connection.Open();
+        SqlDataReader dr = cmd.ExecuteReader();
+        if (dr.HasRows)
+        {
+            while (dr.Read())
+            {
+
+                string Username = dr["Username"].ToString();
+                string Role = dr["Role"].ToString();
+                string status = dr["Status"].ToString();
+                if (status == "True")
+                {
+                    if (!string.IsNullOrEmpty(Username))
+                    {
+                       // Session["ID"] = dr["ID"].ToString();
+                        Session["Username"] = dr["Username"].ToString();
+                        Session["Role"] = dr["Role"].ToString();
+                        txtusername.Text = ""; txtpassword.Text = "";
+
+                        Response.Redirect("Admin/Dashboard.aspx");
+                    }
+                }
+                else
+                {
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "alert", "alert('Login Failed, Activate Your Account First..!!');window.location='Loginpage.aspx'; ", true);
+                    // ClientScript.RegisterStartupScript(this.GetType(), "alert", "HideLabelerror('Login Failed, Activate Your Account First..!!')", true);
+                    txtusername.Text = ""; txtpassword.Text = "";
+                }
+            }
+        }
+        else
+        {
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "alert", "alert('Login Failed, Incorrect Username or Password..!!');window.location='Loginpage.aspx'; ", true);
+            // ClientScript.RegisterStartupScript(this.GetType(), "alert", "HideLabelerror('Login Failed, Incorrect Username or Password..!!')", true);
+            txtusername.Text = ""; txtpassword.Text = "";
+        }
+        cmd.Connection.Close();
+    }
+}
